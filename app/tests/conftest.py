44bd87538a6c7b5662a2637f3808c2e6794535d5
mukulnagar-gammaedge <mukul.nagar@gammaedge.io>
2026-03-09 17:59:45 +0530

    seventeen.7.1 commit

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -4,24 +4,18 @@
 
 
 import pytest
-from unittest.mock import patch
+
 
 @pytest.fixture
 def sample_pdf(tmp_path):
-    """Provide a temporary fake PDF file for testing uploads."""
+    #ye hume ek temperory pdf dega tesing ke liye
     pdf_path = tmp_path / "sample.pdf"
     pdf_path.write_bytes(b"%PDF-1.4\n% Fake PDF content")
     return pdf_path
 
 @pytest.fixture
 def fake_job_id():
-    """Provide a fake job ID for testing job status endpoint."""
+    #yaha humne ek fake id generate ki h 
     return "test-job-123"
 
 
-'''@pytest.fixture(autouse=True)
-def mock_celery_delay():
-    
-    with patch("app.api.routes.process_resume.delay") as mocked:
-        mocked.return_value.id = "fake-job-id"
-        yield mocked'''
